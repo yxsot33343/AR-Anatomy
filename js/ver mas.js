863cfe08eb0b1806227e5d98d307a4e7e076bdc4
yxsot33343 <yasmeilycelesteotiz@gmail.com>
2025-05-21 23:46:39 -0400

    Confirmar

--- js/ver mas.js
+++ js/ver mas.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.models-grid');
   
@@ -239,3 +240,4 @@ document.addEventListener('DOMContentLoaded', () => {
       grid.appendChild(wrapper.firstElementChild);
     });
   });
+
